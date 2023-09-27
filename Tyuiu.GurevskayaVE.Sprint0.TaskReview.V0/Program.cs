934d8f8b56ec91e4346365ecadf4329a184ee278
Tyuiu.GurevskayaVE@outlook.com <Tyuiu.GurevskayaVE@outlook.com>
2023-09-27 21:37:59 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.GurevskayaVE.Sprint0.TaskReview.V0/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GurevskayaVE.Sprint0.TaskReview.V0.Lib;
+
+namespace Tyuiu.GurevskayaVE.Sprint0.TaskReview.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #0 | Выполнила: Гурьевская В.Е. | ПКТб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #0                                                               *");
+            Console.WriteLine("* Тема: Обзор спринта                                                     *");
+            Console.WriteLine("* Задание #Review                                                         *");
+            Console.WriteLine("* Вариант #0                                                              *");
+            Console.WriteLine("* Выполнила: Гурьевская В.Е. | ПКТб-23-2                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Создать код реализации приложения (запрос и вывод данных)                *");
+            Console.WriteLine("* (x + y + z) * 5                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("(x + y + z) * 5");
+
+            Console.Write("Введите x: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите z: ");
+            int z = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(DataService.Calc(x, y, z));
+
+            Console.ReadKey();
+        }
+    }
+}
